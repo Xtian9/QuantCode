779cc4308ea0ad2bd7c4025c26a5ecedd20ebd0a
Xtian9 <cl4010@ic.ac.uk>
2016-04-29 23:45:05 +0200

    fix: y=mx+c

--- portfolios/hedgeratioweights.py
+++ portfolios/hedgeratioweights.py
@@ -10,10 +10,10 @@ class HedgeRatioWeightsPortfolio(Portfolio):
         super(HedgeRatioWeightsPortfolio, self).generate_positions()
         
         # note absolute number of shares cancels out 
-        total_invested = self.x_prices + self.y_prices * self.beta
+        total_invested = self.beta * self.x_prices + self.y_prices# * self.beta
 
-        weight_x = self.x_prices / total_invested
-        weight_y = self.y_prices * self.beta / total_invested
+        weight_x = self.x_prices * self.beta / total_invested
+        weight_y = self.y_prices / total_invested
 
         self.weights.iloc[:, 0] = weight_x
         self.weights.iloc[:, 1] = weight_y
