45770a771de4e7ce4152e2a51231a0244af89685
Xtian9 <cl4010@ic.ac.uk>
2016-07-03 01:41:57 +0200

    add and improve performance plots

--- analysers/performance.py
+++ analysers/performance.py
@@ -2,8 +2,13 @@ from core.stack import *
 from core.baseclasses import Analyser
 from math import sqrt
 from collections import OrderedDict as odict
+import plotting
 import utils
 import os
+
+# Number of periods in a year
+freq_nperiods = {'hourly': 252*6.5, 'daily': 252, 
+                 'weekly': 52, 'monthly': 12, 'annually': 1}
 
 def format_string(value, fmt, pct=False):
     return '{:{}}'.format(value*(100 if pct else 1), fmt) + '%'*pct
@@ -11,87 +16,110 @@ def format_string(value, fmt, pct=False):
 
 class PerformanceAnalyser(Analyser):
 
-    # Number of periods in a year
-    freq_nperiods = {'hourly': 252*6.5, 'daily': 252, 'weekly': 52, 'monthly': 12, 'annually': 1}
-    prec = '.2f'
-    
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
         self.rfrate = 0.04
         self.results = odict()
+        self.figures = []
 
     def begin(self):
         pass
 
     def generate_analysis(self):
-        self.nperiods = PerformanceAnalyser.freq_nperiods[self.frequency]
+        self.nperiods = freq_nperiods[self.frequency]
 
         self.portfolio_returns()
         self.benchmark_returns()
+
+        # Measures
         self.alpha_beta()
         self.sharpe_ratio()
         self.information_ratio()
         self.drawdown()
         self.log_results()
+
+        # Plots
         self.plot_equity_curve()
+        self.plot_rolling_sharpe()
+        self.plot_drawdown()
+        self.plot_top_drawdowns()
+
+    #____________________________________________________________________________||
+    # Input
 
     def portfolio_returns(self):
         self.cumreturns = (1 + self.returns).cumprod() - 1
 
         # annual percentage return, approximation only
         self.annual_return = self.nperiods * self.returns.mean()
-        self.results['APR'] = format_string(self.annual_return, '.2f', True)
+        self.results['APR'] = format_string(self.annual_return, 
+                                            '.2f', True)
 
         self.annual_std = sqrt(self.nperiods) * self.returns.std()
-        self.results['Volatility'] = format_string(self.annual_std, '.2f', True)
+        self.results['Volatility'] = format_string(self.annual_std, 
+                                                   '.2f', True)
 
-        self.results['Total return'] = format_string(self.cumreturns[-1], '.2f', True)
+        self.results['Total return'] = format_string(self.cumreturns[-1],
+                                                     '.2f', True)
 
     def benchmark_returns(self):
         self.returns_bm = self.prices_bm.pct_change().iloc[:,0]
         self.cumreturns_bm = (1 + self.returns_bm).cumprod() - 1
 
-        self.results['Total return bmark'] = format_string(self.cumreturns_bm[-1], '.2f', True)
+        self.results['Total return bmark'] = format_string(
+                                                self.cumreturns_bm[-1],
+                                                '.2f', True)
+
+    #____________________________________________________________________________||
+    # Measures
 
     def alpha_beta(self):
-        self.alpha, self.beta = utils.alpha_beta(self.returns, self.returns_bm)
+        self.alpha, self.beta = utils.alpha_beta(self.returns, 
+                                                 self.returns_bm)
         self.results['Alpha'] = format_string(self.alpha, '.2f')
         self.results['Beta'] = format_string(self.beta, '.2f')
 
     def sharpe_ratio(self):
-        self.sharpe = utils.sharpe_ratio(self.returns, self.nperiods, self.rfrate)
+        self.sharpe = utils.sharpe_ratio(self.returns, self.nperiods, 
+                                         self.rfrate)
         self.results['Sharpe ratio'] = format_string(self.sharpe, '.2f')
 
     def information_ratio(self):
-        self.information_ratio = utils.information_ratio(self.returns, self.returns_bm, self.nperiods)
-        self.results['Information ratio'] = format_string(self.information_ratio, '.2f')
+        self.information_ratio = utils.information_ratio(self.returns, 
+                                               self.returns_bm, self.nperiods)
+        self.results['Information ratio'] = format_string(
+                                                self.information_ratio, '.2f')
 
     def drawdown(self):
-        self.max_dd, self.max_ddd = utils.drawdown(self.cumreturns)
+        self.max_dd = utils.max_drawdown(self.cumreturns)
+        self.max_ddd = utils.max_drawdown_duration(self.cumreturns)
         self.results['Max DD'] = format_string(self.max_dd, '.2f', True)
-        self.results['Max DD duration'] = format_string(self.max_ddd, 'd')
+        self.results['Max DD duration'] = format_string(self.max_ddd, '.0f')
+
+    #____________________________________________________________________________||
+    # Plots
 
     def plot_equity_curve(self):
-        #df = pd.DataFrame(index=self.cumulative_return.index)
-        #df['Equity'] = self.cumulative_return
-        #df['Benchmark'] = self.benchmark_cumulative_return
-        #df = pd.DataFrame(dict(Equity=self.cumulative_return, 
-        #                       Benchmark=self.benchmark_cumulative_return))
+        fig, ax = plotting.plot_equity_curve(self.cumreturns, 
+                                             self.cumreturns_bm)
+        self.save_fig(fig, 'equitycurve')
 
-        fig = plt.figure()
-        fig.patch.set_facecolor('white')
-        ax = fig.add_subplot(111, ylabel='Portfolio value growth (%)')
-        ax.set_title('Equity curve')
-        self.cumreturns.plot(ax=ax, label='Equity')
-        self.cumreturns_bm.plot(ax=ax, label='Benchmark')
-        ax.legend(loc='best', frameon=False)
-        ax.axhline(0, linestyle='--', color='black', lw=2)
+    def plot_rolling_sharpe(self, window=6):
+        fig, ax = plotting.plot_rolling_sharpe(self.returns, self.nperiods, 
+                                               self.rfrate, window)
+        self.save_fig(fig, 'rollingsharpe')
 
-        #df.plot()
-        #if self.full:
-        #   print "save to output"
-        #else:
-        plt.show()
+    def plot_drawdown(self):
+        fig, ax = plotting.plot_drawdown(self.cumreturns)
+        self.save_fig(fig, 'drawdown')
+
+    def plot_top_drawdowns(self, ntop=5):
+        for ddtype, (fig, ax) in plotting.plot_top_drawdowns(
+                                    self.cumreturns, ntop).iteritems():
+            self.save_fig(fig, 'drawdown_top{}_{}'.format(ntop, ddtype))
+
+    #____________________________________________________________________________||
+    # Output
 
     def log_results(self):
         fout = open(os.path.join(self.outdir, 'log.txt'), 'w')
@@ -104,4 +132,8 @@ class PerformanceAnalyser(Analyser):
             s = '{:20} {}'.format(metric, value)
             print s
             fout.write(s+"\n")
+
+    def save_fig(self, fig, name, ext='.png'):
+        fig.savefig(os.path.join(self.outdir, name+ext))
+        self.figures.append(fig)
 
