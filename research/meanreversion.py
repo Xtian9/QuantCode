620583daba0204a2398acbd1b7a87a45cf071730
Xtian9 <cl4010@ic.ac.uk>
2016-07-29 02:51:32 +0200

    update meanreversion research

--- research/meanreversion.py
+++ research/meanreversion.py
@@ -1,23 +1,29 @@
 from core.stack import *
 from datahandler import DataHandler
-from math import log
+from utils import plotting
 import statsmodels.tsa.stattools as ts
+#import statsmodels.formula.api as sm
+import math
 
 
 ## __________________________________________________________________________||
 ## Config
 
-#symbols    = ['SPY']#,'IWM']
+#symbols = ['GOOG']
+#qcodes = ['GOOG/NASDAQ_GOOG']
+#date_start = "2000-01-01"
+#date_end   = "2013-01-01"
+
 symbols    = ['AREX','WLL']
-#qcodes     = ['GOOG/NYSEARCA_'+s for s in symbols]
 qcodes     = ['GOOG/NASDAQ_AREX','GOOG/NYSE_WLL']
 date_start = "2012-01-01"
 date_end   = "2013-01-01"
+
 frequency  = "daily"
 datas      = ['Close']
 trade_time = 'Close'
 
-TEST = 'adf'
+#TEST = 'adf'
 
 
 ## __________________________________________________________________________||
@@ -25,23 +31,26 @@ TEST = 'adf'
 
 def main():
     
-    data_handler = DataHandler(symbols, qcodes, date_start, date_end, frequency, datas)
+    data_handler = DataHandler(symbols, qcodes, date_start, date_end, 
+                               frequency, datas)
     datas_symbols = data_handler.generate_data()
     
-    #prices = datas_symbols['Close'].iloc[:,0]
-    prices = datas_symbols[trade_time]
+    if len(symbols) == 1:
+        prices = datas_symbols[trade_time].iloc[:,0]
+    else:
+        prices = datas_symbols[trade_time]
 
-    plot_time_series(prices)
-    plot_scatter(prices)
+    #plot_time_series(prices)
+    
+    #___ Single asset ___#
+    #print adf(prices)
+    #print hurst(prices, True)
+
+    #___ Multiple assets ___#
+    #plot_scatter(prices)
     plot_residuals(prices)
-    print cadf(prices)
-
-    #prices = pd.Series(np.log(np.cumsum(np.random.randn(100000)+100)))
-    #plt.plot(prices);plt.show()
-
-    #adf(prices)
-    #prices = np.log(prices)
-    #hurst(prices)
+    #print cadf(prices)
+    print halflife(residuals(prices))
 
 
 ## __________________________________________________________________________||
@@ -70,16 +79,36 @@ def cadf(prices):
     return adf(residuals(prices))
 
 
-def hurst(prices):
+def hurst(prices, plot=False):
     """
     Hurst exponent test
     """
     prices = np.log(prices)
-    lags = range(1,100)
-    means = [((prices.shift(-lag) - prices)**2).mean() for lag in lags]
-    plt.scatter(np.log(lags), np.log(means)); plt.show()
-    linfit = np.polyfit(np.log(lags), np.log(means), 1)
-    hurst = linfit[0] / 2
+
+    lags = range(1, 100)
+
+    # Variance of differences for each lag tau
+    variances = [((prices.shift(-lag) - prices)**2).mean() for lag in lags]
+
+    loglags = np.log(lags)
+    logvars = np.log(variances)
+
+    # Fit a straight line
+    b, a = np.polyfit(loglags, logvars, 1)
+
+    if plot:
+        fig, ax = plt.subplots()
+
+        plt.scatter(loglags, logvars)
+        plt.plot(loglags, a + b * loglags, lw=2, color='orange')
+
+        plotting.style_default(ax, fig,
+                               xlabel='log(Lag)', ylabel='log(Variance)')
+        plt.show()
+
+    # Gradient is 2H
+    hurst = b / 2
+
     return hurst
 
 
@@ -97,8 +126,11 @@ def halflife(prices):
     """
     prices_lag1 = prices.shift(1)
     delta_prices = prices - prices_lag1
-    lambda_ = pd.ols(x=prices_lag1, y=delta_prices)
-    return -log(2) / lambda_
+    
+    # EPChan2 Eq. (2.5)
+    lamda = pd.ols(x=prices_lag1, y=delta_prices).beta.x
+
+    return -math.log(2) / lamda
 
 
 def ols(prices):
@@ -130,23 +162,50 @@ def residuals(prices, beta=None):
 ## Plotting tools
 
 def plot_time_series(prices, *names):
-    if names: 
-        assert len(prices.columns) == len(names), "AH"
-    else:
-        names = prices.columns
+    fig, ax = plt.subplots()
 
-    # add axes labels, title, etc
-    prices.plot()
+    prices.plot(ax=ax)
+    plotting.style_default(ax, fig, ylabel='Price')
+
     plt.show()
         
 
-def plot_scatter(prices, *names):
-    plt.scatter(prices.iloc[:,0], prices.iloc[:,1])
+def plot_scatter(prices):
+    assert len(prices.columns) == 2
+
+    xprices = prices.iloc[:, 0]
+    yprices = prices.iloc[:, 1]
+
+    fig, ax = plt.subplots()
+    ax.scatter(xprices, yprices, label=None)
+
+    b, a = ols(prices).beta
+    #print sm.ols(formula='{} ~ {}'.format(*reversed(prices.columns)),
+    #             data=prices).fit().summary()
+
+    xmin, xmax = xprices.min(), xprices.max()
+    axrange = np.arange(xmin, xmax, 0.01 * (xmax - xmin))
+
+    ax.plot(axrange, a + b * axrange, 
+            lw=4, color='orange', 
+            label='y = {:.2f}x + {:.2f}'.format(b, a))
+
+    axlabel = 'Price of {}'
+    plotting.style_default(ax, fig,
+                           xlabel=axlabel.format(prices.columns[0]), 
+                           ylabel=axlabel.format(prices.columns[1]))
+    
     plt.show()
 
 
 def plot_residuals(prices):
-    residuals(prices).plot()
+    fig, ax = plt.subplots()
+
+    #residuals(prices).plot()
+    ax.plot(residuals(prices))
+
+    plotting.style_default(ax, fig, ylabel='Residual')                           
+
     plt.show()
 
 
